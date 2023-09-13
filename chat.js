6cf435fddb823ab69aecd5848417d72c94584c16
Kamlesh Das <daskamlesh677@gmail.com>
2023-09-13 23:18:38 +0530

    code converter deployed backend

--- chat.js
+++ chat.js
@@ -1,20 +1,19 @@
 const  OpenAIApi  = require('openai');
 require("dotenv").config();
-const { AbortController } = require('abort-controller'); 
+
 
 const openai = new OpenAIApi({
     apiKey: process.env.API_KEY,
 });
 
 async function convertCode(code,language) {
-    const controller = new AbortController(); // Create an AbortController
-    const signal = controller.signal;
+   
  
     const response=await openai.chat.completions.create({
         model:"gpt-3.5-turbo",
         messages:[{"role":"user","content":`Translate the following code to ${language}:\n\n${code}`}],
         max_tokens:4000,
-        signal
+     
     });
 
     return response.choices[0].message;
@@ -22,28 +21,26 @@ async function convertCode(code,language) {
 
 async function debugCode(code) {
 
-    const controller = new AbortController(); // Create an AbortController
-    const signal = controller.signal;
+    
    
     const response=await openai.chat.completions.create({
         model:"gpt-3.5-turbo",
         messages:[{"role":"user","content":`Debug the following code:\n\n${code} \n \n if there is any issue with the code write what is the issue and correct it and show the correct code`}],
         max_tokens:4000,
-        signal
+      
     });
 
     return response.choices[0].message;
 }
 
 async function checkCodeQuality(code) {
-    const controller = new AbortController(); // Create an AbortController
-    const signal = controller.signal;
+   
    
     const response=await openai.chat.completions.create({
         model:"gpt-3.5-turbo",
         messages:[{"role":"user","content":`Check the quality of the following code:\n\n${code}\n give strong code if the code quality is poor`}],
         max_tokens:4000,
-        signal
+      
     });
 
     return response.choices[0].message;
